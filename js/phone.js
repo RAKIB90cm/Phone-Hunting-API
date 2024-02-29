32323d3682fd2d9075e2d7d27f20fb1ca27ff2a3
RAKIB90cm <niru90x@gmail.com>
2024-02-29 21:04:23 +0600

    second commit

--- js/phone.js
+++ js/phone.js
@@ -1,7 +1,29 @@
 const loadData = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
     const data = await res.json();
-    const phones=data.data;
-    console.log(phones);
+    const phones = data.data;
+    displayPhone(phones)
+    // console.log(phones);
+}
+
+const displayPhone = (phones) => {
+    const phoneContainer = document.getElementById('phone-container');
+    phones.forEach(phones => {
+        console.log(phones.image)
+        const div = document.createElement('div');
+        div.classList.add = `card card-compact w-96 bg-base-100 shadow-xl`;
+        div.innerHTML = `
+        <figure><img src="${phones.image}"
+                            alt="Shoes" /></figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+        `;
+        phoneContainer.appendChild(div);
+    });
 }
 loadData()
